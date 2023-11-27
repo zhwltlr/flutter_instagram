19b2d9e2c67f64beb4329af97c0e824a5b9b2370
zhwltlr <vyoungv7340@naver.com>
2023-11-27 21:18:07 +0900

    error fixed

--- android/app/src/main/kotlin/com/example/flutter_instagram/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_instagram/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.flutter_instagram
 
-import io.flutter.embedding.android.FlutterActivity
+class MainActivity: FlutterActivity()
 
-class MainActivity: FlutterActivity() {
-}
+open class FlutterActivity
